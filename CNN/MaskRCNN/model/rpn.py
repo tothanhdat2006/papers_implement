3f0a1b390a0fa5d9f8abda9a3ac44f33324eadfd
ToThanhDat <tothanhdat1421@gmail.com>
2025-03-05 22:29:38 +0700

    Init Mask R-CNN

---
+++ CNN/MaskRCNN/model/rpn.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+
+class RPNHead(nn.Module):
+    def __init__(self):
+        super().__init__()
---
+++ CNN/MaskRCNN/model/utils.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+
+class AnchorGenerator(nn.Module):
+    def __init__(self):
+        super().__init__()
