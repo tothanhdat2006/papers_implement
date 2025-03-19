1de38aa76c3ab114d5c78c6df499106f6512444f
ToThanhDat <tothanhdat1421@gmail.com>
2025-03-19 21:03:11 +0700

    Fix UNet validation script

--- CNN/UNet/evaluate.py
+++ CNN/UNet/evaluate.py
@@ -5,30 +5,35 @@ from tqdm import tqdm
 from utils.dice_score import multiclass_dice_coeff, dice_coeff
 
 @torch.inference_mode()
-def evaluate(model, dataloader, device, amp):
-    model.eval() # Set to evaluation mode
-    test_loss = 0.0
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
 
-    with torch.no_grad():
-        for batch in dataloader:
-            images, masks_true = batch['image'], batch['mask']
-            images = images.to(device, dtype=torch.float32, memory_format=torch.channels_last)
-            masks_true = masks_true.to(device, dtype = torch.long)
-            masks_pred = model(images)
+from utils.dice_score import multiclass_dice_coeff, dice_coeff
+
+@torch.inference_mode()
+def evaluate(model, dataloader, device, amp):
+    model.eval()
+    num_val_batches = len(dataloader)
+    dice_score = 0
+
+    with torch.autocast(device.type if device.type != 'mps' else 'cpu', enabled=amp):
+        for batch in tqdm(dataloader, total=num_val_batches, desc='Validation round', unit='batch', leave=False):
+            image, mask_true = batch['image'], batch['mask']
+            image = image.to(device=device, dtype=torch.float32, memory_format=torch.channels_last)
+            mask_true = mask_true.to(device=device, dtype=torch.long)
+
+            mask_pred = model(image)
 
             if model.n_classes == 1:
-                assert masks_true.min() >= 0 and masks_true.max() <= 1, 'True mask indices should be in [0, 1]'
-                masks_pred = (F.sigmoid(masks_pred) > 0.5).float()
-                # Dice score: (2 * |X| * |Y|) / (|X| + |Y|) 
-                test_loss += dice_coeff(masks_pred, masks_true, reduce_batch_first=False)
+                assert mask_true.min() >= 0 and mask_true.max() <= 1, 'True mask indices should be in [0, 1]'
+                mask_pred = (F.sigmoid(mask_pred) > 0.5).float()
+                dice_score += dice_coeff(mask_pred, mask_true, reduce_batch_first=False)
             else:
-                assert masks_true.min() >= 0 and masks_true.max() < model.n_classes, 'True mask indices should be in [0, n_classes)'
-                masks_true = F.one_hot(masks_true, model.n_classes).permute(0, 3, 1, 2).float()
-                masks_pred = F.one_hot(masks_pred.argmax(dim = 1), model.n_classes).permute(0, 3, 1, 2).float()
-                # Dice score
-                test_loss += multiclass_dice_coeff(masks_pred[:, 1:], masks_true[:, 1:], reduce_batch_first=False)
+                assert mask_true.min() >= 0 and mask_true.max() < model.n_classes, 'True mask indices should be in [0, n_classes['
+                mask_true = F.one_hot(mask_true, model.n_classes).permute(0, 3, 1, 2).float()
+                mask_pred = F.one_hot(mask_pred.argmax(dim=1), model.n_classes).permute(0, 3, 1, 2).float()
+                dice_score += multiclass_dice_coeff(mask_pred[:, 1:], mask_true[:, 1:], reduce_batch_first=False)
 
-    test_loss = test_loss / max(len(dataloader), 1)
-
-    model.train() # Set back to training mode
-    return test_loss
+    model.train()
+    return dice_score / max(num_val_batches, 1)
